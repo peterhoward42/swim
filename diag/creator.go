a332f714c5142dccb7f9ae7795a45949c9622588
peterhoward42 <peterhoward42@gmail.com>
2019-05-04 00:05:30 +0100

    Define attributes for diagram drawing event types.
    For example, the lane lines between which an interaction line should be
    drawn. So that the collaborating types can get the information they need to
    make spacing and positioning decisions.

---
+++ diag/creator.go
@@ -0,0 +1,7 @@
+package diag
+
+// Creator is the top level entry point orchestrator for the diag package.
+// It is capable of consuming a sequence of dslmodel.Statement(s), and 
+// producing the corresponding graphics.Model.
+type Creator struct {
+}
--- diag/events.go
+++ diag/events.go
@@ -17,36 +17,50 @@ Types are used to represent these drawing mandates so that:
 
 package diag
 
+import (
+)
+
 // EndBox is an instruction to close-off or terminate the drawing one of the
 // thin vertical boxes that sits on a lane to emit and receive interactions.
 type EndBox struct {
+	whichBox *StartBox
 }
 
 // Interaction is an instruction to draw a horizontal line from one lane
 // to another.
 type Interaction struct {
+	fromLane *LaneLine
+	toLane *LaneLine
+	dashed bool
 }
 
-// Label is an instruction to create a multi-line label.
-type Label struct {
+// InteractionLabel is an instruction to create a multi-line label that belongs
+// to an Interaction object. I.e. the horizontal line it must sit above.
+type InteractionLabel struct {
+	labelSegments []string
+	interaction Interaction
 }
 
 // LaneLine is an instruction to draw the vertical dashed line that
 // represents one lane.
 type LaneLine struct {
+	laneTitleBox *LaneTitleBox
 }
 
 // LaneTitleBox is an instruction to draw one of the lane title boxes that
 // sit along the top of the diagram.
 type LaneTitleBox struct {
+	labelSegments []string
 }
 
-// SelfLoop is an instruction to draw the series of lines required to
+// SelfInteraction is an instruction to draw the series of lines required to
 // represent an internal interaction on a lane. I.e. 3 sides of a rectangle.
-type SelfLoop struct {
+type SelfInteraction struct {
+	lane *LaneLine
 }
 
 // StartBox is an instruction to start drawing one of the thin vertical boxes
 // that sits on a lane to emit and receive interactions.
 type StartBox struct {
+	lane *LaneLine
 }
