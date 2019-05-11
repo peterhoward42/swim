4926c90da578be563801ae5b9ae91b43e446a6c4
peterhoward42 <peterhoward42@gmail.com>
2019-05-11 11:01:40 +0100

    Output .png images from diag.Creator tests (when env var set).

--- diag/creator_test.go
+++ diag/creator_test.go
@@ -1,9 +1,11 @@
 package diag
 
 import (
+	"os"
 	"testing"
 
 	"github.com/peterhoward42/umli/parser"
+	"github.com/peterhoward42/umli/export"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,6 +16,13 @@ Tests to have
 	o  a lane gets a title box
 */
 
+// When the environment variable UMLI_VISUAL_TESTS is set to "true",
+// some of the tests in this module, output .png images in /tmp for visual
+// inspection instead of programmatic inspection of what Creator has produced.
+func visualTestMode() bool {
+	return os.Getenv("UMLI_VISUAL_TESTS") == "true"
+}
+
 func TestCreateRunsWithoutCrashing(t *testing.T) {
 	statements := parser.MustCompileParse(parser.ReferenceInput)
 	width := 200
@@ -22,13 +31,18 @@ func TestCreateRunsWithoutCrashing(t *testing.T) {
 	creator.Create()
 }
 
-func TestForLaneTitleBoxWhenOnlyThingPresentIsOneLane(t *testing.T) {
+func TestWhenTheOnlyThingPresentIsOneLaneYouGetALaneTitleBox(t *testing.T) {
 	assert := assert.New(t)
 	statements := parser.MustCompileParse("lane A foo")
-	width := 200
-	fontHeight := 3.5
+	width := 2000
+	fontHeight := 20.0
 	creator := NewCreator(width, fontHeight, statements)
 	graphicsModel := creator.Create()
-	prims := graphicsModel.Primitives
-	assert.Len(prims.Lines, 4)
+	if visualTestMode() {
+		err := export.CreatePNG("/tmp/one-lane.png", graphicsModel)
+		assert.NoError(err)
+	} else {
+		prims := graphicsModel.Primitives
+		assert.Len(prims.Lines, 4)
+	}
 }
